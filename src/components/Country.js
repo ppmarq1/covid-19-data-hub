0252cc084d786a9ef279083e6a8d581b8d7c9342
Pedro Marques <ppmarq1@gmail.com>
2022-06-28 13:53:10 +0100

    Update component

---
+++ src/components/Country.js
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+
+const Country = ({
+  country: {
+    country,
+    cases,
+    countryInfo: { flag },
+  },
+  index,
+}) => {
+  const bgColor = (fade = 'ff') =>
+    `#${
+      (index + 1) % 4 === 2 || (index + 1) % 4 === 3 ? '373b44' : '4286f4'
+    }${fade}`;
+  return (
+    <div
+      data-testid="countryItem"
+      className="countryDetails"
+      style={{
+        backgroundImage: `linear-gradient(to right, #4286f4, ${bgColor(
+          'aa',
+        )} 40%, ${bgColor('cc')} 65%, ${bgColor()} 80%), url(${flag})`,
+        backgroundSize: '33% 98%',
+      }}
+    >
+      <div className="countryCount">
+        <h4 className="countryHeader">
+          <span className="countryTitle">{country}</span>
+        </h4>
+        <span className="countryFigures">{cases.toLocaleString()}</span>
+      </div>
+    </div>
+  );
+};
+
+Country.propTypes = {
+  country: PropTypes.shape({
+    country: PropTypes.string.isRequired,
+    cases: PropTypes.number.isRequired,
+    countryInfo: PropTypes.shape({
+      flag: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+export default Country;
